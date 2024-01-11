8cdbcd0511cdc138a0fcde3914d50cc33a562498
dayvar14 <danielayvar@outlook.com>
2024-01-11 09:55:24 -0600

    fix: linting issues

--- src/components/drawsteel/initiativeList.tsx
+++ src/components/drawsteel/initiativeList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 import { Token } from '../../obr/tokens.ts'
 import { PlayerRole, PlayerState } from '../../obr/player.ts'
