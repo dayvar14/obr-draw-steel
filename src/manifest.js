2e3c1678aead17586d1e23c0f791678bfd0dad9c
dayvar14 <danielayvar@outlook.com>
2024-01-16 11:12:23 -0600

    ci: change application name depending on env

--- src/manifest.js
+++ src/manifest.js
@@ -3,8 +3,24 @@ import { APP_HEIGHT, APP_WIDTH } from './config.js'
 
 const argument = process.argv[2]
 
+const environment = process.env.OBR_DEPLOYMENT_ENVIRONMENT
+
+let applicationName = 'Draw Steel! (Test)'
+
+switch (environment) {
+  case 'production':
+    console.log('Running in a production environment.')
+    applicationName = 'Draw Steel!'
+    break
+
+  case 'development':
+    console.log('Running in development environment.')
+    applicationName = 'Draw Steel! (Dev)'
+    break
+}
+
 const manifest = {
-  name: 'Draw Steel!',
+  name: applicationName,
   version: '0.4.0',
   author: 'Daniel Ayvar',
   homepage_url: 'https://github.com/dayvar14/obr-draw-steel',
