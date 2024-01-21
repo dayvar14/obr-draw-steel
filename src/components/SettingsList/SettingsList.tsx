f03e97324aabf1d52ab7950475d4ae6fdd1e9af4
dayvar14 <danielayvar@outlook.com>
2024-01-21 12:15:42 -0600

    fix: broken theme for settings

--- src/components/SettingsList/SettingsList.tsx
+++ src/components/SettingsList/SettingsList.tsx
@@ -238,7 +238,7 @@ const SettingsList = () => {
               >
                 create an issue in Github
               </a>{' '}
-              and/or @StankPapaya on the{' '}
+              and/or @StankyPapaya on the{' '}
               <a
                 className='rounded-square-icon-button large'
                 href='https://discord.com/channels/795808973743194152/1082460044731371591'
