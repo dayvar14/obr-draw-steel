a11f288dac356f581474715a07ab8874b8588d80
Quentin Fulsher <qfulsher@gmail.com>
2024-08-12 12:39:16 -0700

    feat: double clicking sub list item row triggers centerPlayerOnTokens action

--- src/components/InitiativeList/InitiativeSubListItem.tsx
+++ src/components/InitiativeList/InitiativeSubListItem.tsx
@@ -129,6 +129,7 @@ const InitiativeSubListItem: React.FC<{
       draggable={true}
       onDragStart={onDragStart}
       onDragEnd={onDragEnd}
+      onDoubleClick={handleDoubleClick}
     >
       <div className={clsx(['sub-list-item-token', { 'no-turn': !hasTurn }])}>
         <img
@@ -137,7 +138,6 @@ const InitiativeSubListItem: React.FC<{
           onClick={(event: React.MouseEvent<HTMLDivElement>) => {
             handleClick(event)
           }}
-          onDoubleClick={handleDoubleClick}
           onMouseEnter={() => {
             setMouseOverToken(true)
           }}
